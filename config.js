0989fa6e884ee0c13ff7922664ecb05aba120bb0
duxue <1479605026@qq.com>
2020-03-04 22:37:12 +0800

    添加数据库

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DB_HOST:'localhost',
+    DB_USER:'root',
+    DB_PASS:'dx971121',
+    DB_NAME:'graduation'
+}
--- server.js
+++ server.js
@@ -10,8 +10,26 @@ server.use(body({
 	uploadDir:path.resolve(__dirname,'static/upload')
 }))
 
-server.listen (8080)
+//数据库
+server.context.db = require('./lib/database')
+server.listen (8080);
 
+// //统一处理
+// router.use(async (ctx, next)=>{
+// 	try{
+// 		await next()
+// 	}catch(e){
+// 		ctx.state = 500;
+// 		ctx.body = 'Internet server error'
+// 	}
+// })
+
+server.use(async ctx =>{
+	let data = await ctx.db.query('SELECT　* FROM Recommend');
+	ctx.body = data;
+})
+
+//路由和static
 let router = new Router();
 static(router)
 router.use('/',require('./routes/www'))
