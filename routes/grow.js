c112d5a29fb2935fe55b1dd25b063cbeb4c40d87
duxue <1479605026@qq.com>
2020-03-08 21:53:34 +0800

    fix

--- routes/grow.js
+++ routes/grow.js
@@ -3,10 +3,10 @@ const Router=require('koa-router');
 let router=new Router();
 
 router.get('/', async ctx=>{
-  await ctx.render('grow', {
+  await ctx.render('article-list', {
       HTTP_ROOT: ctx.config.HTTP_ROOT,
       errmsg: ctx.query.errmsg,
-      // obj:data
+      obj:{}
     });
   })
 module.exports=router.routes();
