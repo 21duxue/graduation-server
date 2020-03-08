9ea94d5dd01e0f1b71a960b2f3253639810ff1e5
duxue <1479605026@qq.com>
2020-03-08 11:07:14 +0800

    fix db

--- routes/grow.js
+++ routes/grow.js
@@ -4,19 +4,15 @@ const db=require('../lib/database');
 let router=new Router();
 
 router.get('/', async ctx=>{
-  await db.query("SELECT * FROM Grow;",((err,res)=>{
-      if(err){
-        console.log(err)
-      }else{
-        ctx.render('grow', {
-          HTTP_ROOT: ctx.config.HTTP_ROOT,
-          errmsg: ctx.query.errmsg,
-          obj:res
-        });
-      }
-    }))
-    
-});
+  db.query("SELECT * FROM Grow;").then(res=>{
+    ctx.body = res
+  })
+  //   ctx.render('grow', {
+  //     HTTP_ROOT: ctx.config.HTTP_ROOT,
+  //     errmsg: ctx.query.errmsg,
+  //     // obj:res
+  //   });
+  // });
 
 
 module.exports=router.routes();
